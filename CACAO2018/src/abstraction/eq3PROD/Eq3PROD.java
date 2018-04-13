1377b46b0d9d7eaf0af0b133e3c18976321b5d96
Muikhi <Muikhi@172.18.31.211>
2018-04-13 16:56:16 +0200

    1

--- CACAO2018/src/abstraction/eq3PROD/Eq3PROD.java
+++ CACAO2018/src/abstraction/eq3PROD/Eq3PROD.java
@@ -1,7 +1,9 @@
 package abstraction.eq3PROD;
 
+import abstraction.eq1DIST.IVenteConso;
 import abstraction.eq2PROD.IProducteurCacao;
 import abstraction.fourni.Acteur;
+import abstraction.fourni.Monde;
 
 public class Eq3PROD implements Acteur, IProducteurCacao {
 private int stock;
@@ -20,6 +22,8 @@ public Eq3PROD() {
 		int prod = (int)(Math.random()*2000);
 		this.stock = this.stock+prod;
 		System.out.println(" eq 3 prod de "+prod+" --> stock="+this.stock);
+		IVenteConso vendeur = (IVenteConso) (Monde.LE_MONDE.getActeur("Eq6DIST"));
+		vendeur.sell(100);
 	}
 
 	@Override
