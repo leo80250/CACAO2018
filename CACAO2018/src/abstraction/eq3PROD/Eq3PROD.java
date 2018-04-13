7e5fb1d8a7aedd0ccb3ad6caab7c82e17b509136
clair <clair@172.18.31.204>
2018-04-13 14:47:30 +0200

    Préparation 1

--- CACAO2018/src/abstraction/eq3PROD/Eq3PROD.java
+++ CACAO2018/src/abstraction/eq3PROD/Eq3PROD.java
@@ -1,5 +1,19 @@
 package abstraction.eq3PROD;
 
-public class Eq3PROD {
+import abstraction.fourni.Acteur;
+
+public class Eq3PROD implements Acteur {
+
+	@Override
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void next() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
