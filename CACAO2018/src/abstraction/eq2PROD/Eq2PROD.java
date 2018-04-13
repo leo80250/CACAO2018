d7c1f813bc23d543b724e6e44084c34efe526fae
Agathe Chevalier <Agathe Chevalier@172.17.3.219>
2018-04-13 14:59:06 +0200

    interface équipe 2

---
+++ CACAO2018/src/abstraction/eq2PROD/Eq2PROD.java
@@ -0,0 +1,19 @@
+package abstraction.eq2PROD;
+
+import abstraction.fourni.Acteur;
+
+public class Eq2PROD implements Acteur {
+
+	@Override
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return "Eq2PROD";
+	}
+
+	@Override
+	public void next() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
