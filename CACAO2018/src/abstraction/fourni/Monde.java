8ae447f43bdc4bf60f8781d4875f8af899685082
Juliette <Juliette@172.17.2.240>
2018-04-13 16:14:18 +0200

    peupler eq5

--- CACAO2018/src/abstraction/fourni/Monde.java
+++ CACAO2018/src/abstraction/fourni/Monde.java
@@ -3,6 +3,8 @@ package abstraction.fourni;
 import java.util.ArrayList;
 import java.util.Observable;
 
+import abstraction.eq5TRAN.Eq5TRAN;
+
 /**
  * Classe modelisant "le monde" vu comme un regroupement d'acteurs, 
  * d'indicateurs et de journaux. 
@@ -49,7 +51,8 @@ public class Monde extends Observable {
 	 * le corps de cette methode
 	 */
 	public void peupler() {
-		// Methode a redefinir
+		Eq5TRAN eq5 = new Eq5TRAN();
+		this.ajouterActeur(eq5);
 	}
 	/**
 	 * @return Retourne le numero de l'etape en cours.
