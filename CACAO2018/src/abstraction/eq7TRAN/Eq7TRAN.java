24723b32b56d6a22ecbb5813bb8be9c54354f69b
leofa <leofa@172.16.124.238>
2018-04-13 14:45:06 +0200

    implements acteurs

--- CACAO2018/src/abstraction/eq7TRAN/Eq7TRAN.java
+++ CACAO2018/src/abstraction/eq7TRAN/Eq7TRAN.java
@@ -1,5 +1,19 @@
 package abstraction.eq7TRAN;
 
-public class Eq7TRAN {
+import abstraction.fourni.Acteur;
 
+public class Eq7TRAN implements Acteur {
+
+	@Override
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void next() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
