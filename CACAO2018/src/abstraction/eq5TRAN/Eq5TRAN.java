0dcacfb8479b415388b15361956370be9304b58a
colozz <colozz@Paarthurnax>
2018-04-13 15:39:56 +0200

    Implementation de ITransformateur

--- CACAO2018/src/abstraction/eq5TRAN/Eq5TRAN.java
+++ CACAO2018/src/abstraction/eq5TRAN/Eq5TRAN.java
@@ -1,9 +1,14 @@
 package abstraction.eq5TRAN;
 
+import abstraction.eq4TRAN.ITransformateur;
 import abstraction.fourni.Acteur;
 
-public class Eq5TRAN implements Acteur {
+public class Eq5TRAN implements Acteur, ITransformateur {
 
+	private int stock=1000;
+	private float prix=1.0f;
+	private float banque=0.0f;
+	
 	@Override
 	public String getNom() {
 		return "Eq5TRAN";
@@ -12,6 +17,13 @@ public class Eq5TRAN implements Acteur {
 	@Override
 	public void next() {
 		System.out.println("L'équipe 5 est présente");		
+	}
+
+	@Override
+	public void sell(int q) {
+		if(q>stock) return;
+		stock-=q;
+		banque+=q*prix;
 	}
 
 }
