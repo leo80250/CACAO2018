4a2d15671f0917cf95dd46217c590a8a613c6d7b
Charles <Charles@172.17.3.137>
2018-04-13 15:11:10 +0200

    EQ4

---
+++ CACAO2018/src/abstraction/eq4TRAN/Eq4TRAN.java
@@ -0,0 +1,19 @@
+package abstraction.eq4TRAN;
+
+import abstraction.fourni.Acteur;
+
+public class Eq4TRAN implements Acteur{
+
+	@Override
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return "Eq4TRAN";
+	}
+
+	@Override
+	public void next() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
