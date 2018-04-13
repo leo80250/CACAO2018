30391d807ba2274431c3ca9293dbaac326c0d9fb
Agathe Chevalier <Agathe Chevalier@172.17.3.219>
2018-04-13 16:36:07 +0200

    stock et production

--- CACAO2018/src/abstraction/eq2PROD/Eq2PROD.java
+++ CACAO2018/src/abstraction/eq2PROD/Eq2PROD.java
@@ -2,18 +2,28 @@ package abstraction.eq2PROD;
 
 import abstraction.fourni.Acteur;
 
-public class Eq2PROD implements Acteur {
-
-	@Override
+public class Eq2PROD implements Acteur, IProducteurCacao {
+	private int stock;
+	public Eq2PROD() {
+		this.stock=10000000;
+	}
+	
 	public String getNom() {
 		// TODO Auto-generated method stub
 		return "Eq2PROD";
 	}
 
-	@Override
 	public void next() {
+		int prod = (int) (Math.random()*1000);
+		this.stock=this.stock+prod;
+		System.out.println("production de "+prod+" : le stock de eq2 est : "+this.stock);
 		// TODO Auto-generated method stub
 		
+	}
+
+	public void sell(int q) {
+		this.stock=this.stock-q;
+		System.out.println("eq2 sell ( "+q+") --> "+this.stock);
 	}
 
 }
