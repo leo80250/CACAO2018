4e467f01fefe36587e952613e7e9fee020dbbe00
petit <petit@172.16.123.84>
2018-04-13 14:59:59 +0200

    fhrufg

---
+++ CACAO2018/src/abstraction/eq6DIST/Eq6DIST.java
@@ -0,0 +1,19 @@
+package abstraction.eq6DIST;
+
+import abstraction.fourni.Acteur;
+
+public class Eq6DIST implements Acteur {
+
+	@Override
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return "Eq6DIST";
+	}
+
+	@Override
+	public void next() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
