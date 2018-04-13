4a13cd4c84d7acb3ff655b0e35a0ddb941d952aa
Muikhi <Muikhi@172.17.2.24>
2018-04-13 16:55:41 +0200

    # WARNING: head commit changed in the meantime
    B

--- CACAO2018/src/abstraction/eq3PROD/Eq3PROD.java
+++ CACAO2018/src/abstraction/eq3PROD/Eq3PROD.java
@@ -4,7 +4,10 @@ import abstraction.eq2PROD.IProducteurCacao;
 import abstraction.fourni.Acteur;
 
 public class Eq3PROD implements Acteur, IProducteurCacao {
-
+private int stock;
+public Eq3PROD() {
+	this.stock= 1000000000;
+}
 	@Override
 	public String getNom() {
 		// TODO Auto-generated method stub
@@ -14,13 +17,16 @@ public class Eq3PROD implements Acteur, IProducteurCacao {
 	@Override
 	public void next() {
 		// TODO Auto-generated method stub
-		
+		int prod = (int)(Math.random()*2000);
+		this.stock = this.stock+prod;
+		System.out.println(" eq 3 prod de "+prod+" --> stock="+this.stock);
 	}
 
 	@Override
 	public void sell(int q) {
 		// TODO Auto-generated method stub
-		
+		this.stock=this.stock-q;
+		System.out.println(" eq 3 vend "+q+" --> stock="+this.stock);
 	}
 	
 }
