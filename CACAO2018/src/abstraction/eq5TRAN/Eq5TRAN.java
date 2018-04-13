0f120029142ff27d7a02f6927170b838c8a63c38
Juliette <Juliette@172.17.2.240>
2018-04-13 14:43:48 +0200

    implementation d'Acteur

--- CACAO2018/src/abstraction/eq5TRAN/Eq5TRAN.java
+++ CACAO2018/src/abstraction/eq5TRAN/Eq5TRAN.java
@@ -1,5 +1,19 @@
 package abstraction.eq5TRAN;
 
-public class Eq5TRAN {
+import abstraction.fourni.Acteur;
+
+public class Eq5TRAN implements Acteur {
+
+	@Override
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void next() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
