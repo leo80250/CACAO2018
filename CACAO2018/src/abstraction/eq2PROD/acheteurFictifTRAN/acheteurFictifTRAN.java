761f01fdff588a428e6352d2ce09c48880deafc6
guillaume <guillaume@GS-T460p>
2018-05-22 10:08:27 +0200

    acheteur fictif

---
+++ CACAO2018/src/abstraction/eq2PROD/acheteurFictifTRAN/acheteurFictifTRAN.java
@@ -0,0 +1,17 @@
+package abstraction.eq2PROD.acheteurFictifTRAN;
+
+import abstraction.fourni.Acteur;
+
+public class acheteurFictifTRAN implements Acteur {
+	
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return "acheteurFictifTRAN";
+	}
+	
+	
+	public void next() {
+		
+	}
+
+}
