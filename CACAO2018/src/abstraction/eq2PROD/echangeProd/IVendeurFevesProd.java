5389b135fa635beea1446e9fbeff6888e01230eb
Agathe Chevalier <Agathe Chevalier@172.17.2.45>
2018-05-22 09:06:59 +0200

    modif explications

--- CACAO2018/src/abstraction/eq2PROD/echangeProd/IVendeurFevesProd.java
+++ CACAO2018/src/abstraction/eq2PROD/echangeProd/IVendeurFevesProd.java
@@ -4,10 +4,10 @@ public interface IVendeurFevesProd {
 	/* getPrix donne le prix de vente des fèves, et le grp3 ne peut nous acheter que des feves de
 	 * qualité moyenne au prix du marché multiplié par 1.06 */
 	public double getPrix();
-	/* acheter prend en compte quantite indiqué par le grp3 et renvoie une quantité :
-	 * -soit la quantité demandé et elle est alors vendue au grp3 au prix de vente du moment*1.06,
-	 * il faut mettre à jour stock et le solde
-	 * -soit la quantité est inférieure : elle est quand même vendue prix*1.06, il faut mettre à 
+	/* acheter prend en compte la quantite demandée par le grp3 et renvoie une quantité :
+	 * -soit la quantité demandée, elle est alors vendue au grp3 au prix de vente du moment*1.06,
+	 * il faut mettre à jour stock et solde
+	 * -soit une quantité inférieure : elle est quand même vendue prix*1.06, il faut mettre à 
 	 * jour stock et solde
 	 * -soit 0 alors il n'y a pas d'échange, rien ne se passe
 	 */
