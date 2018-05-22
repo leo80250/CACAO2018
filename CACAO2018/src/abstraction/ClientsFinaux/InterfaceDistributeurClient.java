3ef2c4885f9f5e619d5b4613e1dbf3c0d91363c2
leovu <leovu@172.17.3.244>
2018-05-22 12:03:12 +0200

    Léo

---
+++ CACAO2018/src/abstraction/ClientsFinaux/InterfaceDistributeurClient.java
@@ -0,0 +1,18 @@
+/**
+ * @author Leo Vuylsteker / Axelle Hermelin 
+ */
+
+package abstraction.ClientsFinaux;
+
+public interface InterfaceDistributeurClient {
+	/**
+	 *
+	 * @param Q
+	 *            : tableau d'entier (2x3) ligne 1: ChocolatBdG ; ChocolatMdG ;
+	 *            ChocolatHdG ligne 2: ConfiserieBdG ; ConfiserieMdG ; ConfiserieHdG
+	 * @return tableau d'entier du même modèle qui correspond à ce que le
+	 *         distributeur vend aux clients.
+	 */
+	public GrilleQuantite commander(GrilleQuantite Q);
+
+}
