2680654533ad695362dcfdece77ab43269e83c87
leofa <leofa@172.16.124.238>
2018-05-22 09:54:37 +0200

    commentaires

--- CACAO2018/src/abstraction/echangeTRANTRAN/IAcheteurPoudre.java
+++ CACAO2018/src/abstraction/echangeTRANTRAN/IAcheteurPoudre.java
@@ -1,15 +1,21 @@
 package abstraction.echangeTRANTRAN;
 
 public interface IAcheteurPoudre {	
+	// Récupérer qui vend quoi, en quelle quantité et qualité et à quel prix auprès du régulateur 
 	public ContratPoudre[] getOffresPubliques();
+	// Pour envoyer ce que l'on souhaite acheter, en quelle quantité... au régulateur
 	public void sendOffresPubliques(ContratPoudre[] offres);
 	
+	// Pour récupérer un devis auprès d'un autre TRAN
 	public ContratPoudre[] getDemandePrivee();
+	// Pour envoyer un devis
 	public void sendDemandePrivee(ContratPoudre[] demandes);
 	
+	// Pour récupérer le contrat final
 	public ContratPoudre[] getOffreFinale();
 	public void sendOffreFinale(ContratPoudre[] contrats);
 	
+	// Pour récupérer le résultat de l'échange
 	public ContratPoudre[] getResultAchats();
 	public void sendResultAchats(ContratPoudre[] contrats);	
 }
