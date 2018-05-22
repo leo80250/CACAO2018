674c6bef785799f9d7b8a5d186108323b706d77b
Charles <Charles@172.17.3.46>
2018-05-22 11:30:14 +0200

    Carlos

---
+++ CACAO2018/src/abstraction/eq4TRAN/VendeurChoco/Methods.java
@@ -0,0 +1,22 @@
+package abstraction.eq4TRAN.VendeurChoco;
+/**
+ * 
+ * @author Etienne
+ *
+ */
+public class Methods {
+	/*
+	 * classe définissant les méthodes nécessaires à l'interface IVendeur Choco
+	 */
+	
+/*
+	public GQte getStock() {
+		
+	}
+	
+	public GQte getLivraison(GQte[] commandes) {
+		GQte commande1 = commandes[0];
+		GQte commande2 = commandes[1];
+		
+	}*/
+}
