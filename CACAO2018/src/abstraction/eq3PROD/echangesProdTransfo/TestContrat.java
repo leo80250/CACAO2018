e500dffca412444820fe219dcbc3f0c9ff7798fb
Grégoire <Grgoire@172.18.71.204>
2018-05-22 11:17:44 +0200

    Ajout du test de Contrar

--- CACAO2018/src/abstraction/eq3PROD/echangesProdTransfo/TestContrat.java
+++ CACAO2018/src/abstraction/eq3PROD/echangesProdTransfo/TestContrat.java
@@ -3,6 +3,15 @@ package abstraction.eq3PROD.echangesProdTransfo;
 public class TestContrat {
 	public static void main(String[] args) {
 		Contrat Test1 = new Contrat();
-		System.out.println(Test1.toString());
+		System.out.println(Test1.toString()+"\n");
+		
+		AcheteurTest ach = new AcheteurTest("TransfoTest");
+		VendeurTest vend = new VendeurTest("ProdTest");
+		
+		Contrat Test2 = new Contrat(2, 1250, 10.0, ach, vend, false);
+		System.out.println(Test2.toString()+"\n");
+		
+		Contrat Test3 = new Contrat(1, 1400, 50.0, ach, vend, true);
+		System.out.println(Test3.toString()+"\n");
 	}
 }
