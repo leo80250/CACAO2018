2b0d4fbc194d0dde637b4fd054566bcaa065d6a4
Grégoire <Grgoire@172.18.71.204>
2018-05-22 11:00:28 +0200

    Suite de l'interface de vente de fèves.

---
+++ CACAO2018/src/abstraction/eq3PROD/echangesProdTransfo/Contrat.java
@@ -0,0 +1,104 @@
+package abstraction.eq3PROD.echangesProdTransfo;
+
+import abstraction.fourni.Acteur;
+
+public class Contrat {
+	
+	private int qualite;
+	private int quantite;
+	private double prix;
+	private IAcheteurFeve transformateur;
+	private IVendeurFeve producteur;
+	private boolean reponse;
+	
+	/**
+	 * Constructeurs d'un Contrat
+	 * @param qualite la qualite des feves du contrat (0 = basse, 1 = moyenne, 2 = haute)
+	 * @param quantite la quantite de feves du contrat (en tonne)
+	 * @param prix le prix du contrat (a la tonne)
+	 * @param transformateur le transformateur qui achete les feves
+	 * @param producteur le producteur qui vend les feves
+	 * @param reponse la reponse confirmant ou infirmant le contrat
+	 */
+	public Contrat(int qualite, int quantite, double prix, IAcheteurFeve transformateur, IVendeurFeve producteur, boolean reponse) {
+		this.qualite = qualite;
+		this.quantite = quantite;
+		this.prix = prix;
+		this.transformateur = transformateur;
+		this.producteur = producteur;
+		this.reponse = reponse;
+	}
+	public Contrat() {
+		this.qualite = 0;
+		this.quantite = 0;
+		this.prix = 0.0;
+		this.transformateur = null;
+		this.producteur = null;
+		this.reponse = false;
+	}
+	
+	/**
+	 * Getes et Seters des differentes variables
+	 */
+	
+	public int getQualite() {
+		return this.qualite;
+	}
+	public void setQualite(int qualite) {
+		this.qualite = qualite;
+	}
+	
+	public int getQuantite() {
+		return this.quantite;
+	}
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+	
+	public double getPrix() {
+		return this.prix;
+	}
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+	
+	public IAcheteurFeve getTransformateur() {
+		return this.transformateur;
+	}
+	public void setTransformateur(IAcheteurFeve transformateur) {
+		this.transformateur = transformateur;
+	}
+	
+	public IVendeurFeve getProducteur() {
+		return this.producteur;
+	}
+	public void setProducteur(IVendeurFeve producteur) {
+		this.producteur = producteur;
+	}
+	
+	public boolean getReponse() {
+		return this.reponse;
+	}
+	public void setReponse(boolean reponse) {
+		this.reponse = reponse;
+	}
+	
+	public String toString() {
+		String [] strqualite = {"basse", "moyenne", "haute"};
+		String rep = "";
+		if (this.reponse) {
+			rep += "\nOffre acceptée";
+		}
+		String acheteur = "";
+		if(this.transformateur != null) {
+			acheteur += ((Acteur)(this.transformateur)).getNom();
+		}
+		String vendeur = "";
+		if(this.producteur != null) {
+			vendeur += ((Acteur)(this.producteur)).getNom();
+		}
+		
+		return "Contrat : "+this.quantite+" tonnes de feve de "+strqualite[this.qualite]+" qualité, à "+this.prix
+				+"€ la tonne.\nAcheteur : "+acheteur+" | Vendeur : "+vendeur+rep;
+	}
+}
---
+++ CACAO2018/src/abstraction/eq3PROD/echangesProdTransfo/TestContrat.java
@@ -0,0 +1,8 @@
+package abstraction.eq3PROD.echangesProdTransfo;
+
+public class TestContrat {
+	public static void main(String[] args) {
+		Contrat Test1 = new Contrat();
+		System.out.println(Test1.toString());
+	}
+}
