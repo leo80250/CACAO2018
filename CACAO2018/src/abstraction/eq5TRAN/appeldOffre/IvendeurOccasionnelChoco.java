344627d8d97ce33f252c7edf9091f8f2bd70475b
Maxim <Maxim@172.16.122.242>
2018-05-25 16:55:08 +0200

    interface vente occas + méthodes

---
+++ CACAO2018/src/abstraction/eq5TRAN/appeldOffre/IvendeurOccasionnelChoco.java
@@ -0,0 +1,8 @@
+package abstraction.eq5TRAN.appeldOffre; /** interface demandes ponctuelles **/
+
+public interface IvendeurOccasionnelChoco {
+	
+	public double getReponse(DemandeAO d); /** retourner Double.max_maxvalue si offre non intéressante, sinon retourner le prix**/
+	
+}
+
