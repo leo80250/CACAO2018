01427d5125de8f0a43e275fc9382fc0b21c7ec08
Juliette <Juliette@172.17.3.170>
2018-06-08 16:45:31 +0200

    petitpush

--- CACAO2018/src/abstraction/eq5TRAN/Eq5TRAN.java
+++ CACAO2018/src/abstraction/eq5TRAN/Eq5TRAN.java
@@ -1,13 +1,10 @@
 package abstraction.eq5TRAN;
 
-import static abstraction.eq5TRAN.Marchandises.FEVES_BQ;
-import static abstraction.eq5TRAN.Marchandises.FEVES_MQ;
-import static abstraction.eq5TRAN.Marchandises.FRIANDISES_MQ;
-import static abstraction.eq5TRAN.Marchandises.POUDRE_HQ;
-import static abstraction.eq5TRAN.Marchandises.POUDRE_MQ;
-import static abstraction.eq5TRAN.Marchandises.TABLETTES_BQ;
-import static abstraction.eq5TRAN.Marchandises.TABLETTES_HQ;
-import static abstraction.eq5TRAN.Marchandises.TABLETTES_MQ;
+import static abstraction.eq5TRAN.Marchandises.*;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 import abstraction.eq5TRAN.appeldOffre.DemandeAO;
 import abstraction.eq5TRAN.appeldOffre.IvendeurOccasionnelChoco;
@@ -18,93 +15,87 @@ import abstraction.fourni.Acteur;
 import abstraction.fourni.Indicateur;
 import abstraction.fourni.Monde;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Eq5TRAN implements Acteur, IAcheteurPoudre, IVendeurPoudre, IvendeurOccasionnelChoco {
 
-    // cf Marchandises.java pour obtenir l'indexation
-    private Indicateur[] productionSouhaitee; // ce qui sort de nos machines en kT
-    private Indicateur[] achatsSouhaites; // ce qu'on achète aux producteurs en kT
-    private float facteurStock; // facteur lié aux risques= combien d'itérations on peut tenir sans réception de feves/poudre
-    private Indicateur[] stocksSouhaites; // margeStock = facteurStock * variationDeStockParIteration, en kT
-    private Indicateur[] stocks; // les vrais stocks en kT
+	// cf Marchandises.java pour obtenir l'indexation
+	private Indicateur[] productionSouhaitee; // ce qui sort de nos machines en kT
+	private Indicateur[] achatsSouhaites; // ce qu'on achète aux producteurs en kT
+	private float facteurStock; // facteur lié aux risques= combien d'itérations on peut tenir sans réception de feves/poudre
+	private Indicateur[] stocksSouhaites; // margeStock = facteurStock * variationDeStockParIteration, en kT
+	private Indicateur[] stocks; // les vrais stocks en kT
 
-    private Indicateur banque; // en milliers d'euros
-    private Indicateur[] prix;
+	private Indicateur banque; // en milliers d'euros
+	private Indicateur[] prix;
 
-    /**
-     * @author: Thomas Schillaci
-     */
-    public Eq5TRAN() {
-        int nbMarchandises = Marchandises.getNombreMarchandises();
-        productionSouhaitee = new Indicateur[nbMarchandises];
-        achatsSouhaites = new Indicateur[nbMarchandises];
-        facteurStock = 3;
-        stocksSouhaites = new Indicateur[nbMarchandises];
-        stocks = new Indicateur[nbMarchandises];
-        prix = new Indicateur[nbMarchandises];
+	/**
+	 * @author: Thomas Schillaci
+	 */
+	public Eq5TRAN() {
+		int nbMarchandises = Marchandises.getNombreMarchandises();
+		productionSouhaitee = new Indicateur[nbMarchandises];
+		achatsSouhaites = new Indicateur[nbMarchandises];
+		facteurStock = 3;
+		stocksSouhaites = new Indicateur[nbMarchandises];
+		stocks = new Indicateur[nbMarchandises];
+		prix = new Indicateur[nbMarchandises];
 
-        productionSouhaitee[FEVES_BQ] = new Indicateur("Production souhaitee de feves BQ", this, 0);
-        productionSouhaitee[FEVES_MQ] = new Indicateur("Production souhaitee de feves MQ", this, 0);
-        productionSouhaitee[TABLETTES_BQ] = new Indicateur("Production souhaitee de tablettes BQ", this, 345);
-        productionSouhaitee[TABLETTES_MQ] = new Indicateur("Production souhaitee de tablettes MQ",this,575);
-        productionSouhaitee[TABLETTES_HQ] = new Indicateur("Production souhaitee de tablettes HQ",this,115);
-        productionSouhaitee[POUDRE_MQ] = new Indicateur("Production souhaitee de poudre MQ",this,50);
-        productionSouhaitee[POUDRE_HQ] = new Indicateur("Production souhaitee de poudre HQ",this,0);
-        productionSouhaitee[FRIANDISES_MQ] = new Indicateur("Production souhaitee de friandises MQ",this,115);
+		productionSouhaitee[FEVES_BQ] = new Indicateur("Production souhaitee de feves BQ", this, 0);
+		productionSouhaitee[FEVES_MQ] = new Indicateur("Production souhaitee de feves MQ", this, 0);
+		productionSouhaitee[TABLETTES_BQ] = new Indicateur("Production souhaitee de tablettes BQ", this, 345);
+		productionSouhaitee[TABLETTES_MQ] = new Indicateur("Production souhaitee de tablettes MQ",this,575);
+		productionSouhaitee[TABLETTES_HQ] = new Indicateur("Production souhaitee de tablettes HQ",this,115);
+		productionSouhaitee[POUDRE_MQ] = new Indicateur("Production souhaitee de poudre MQ",this,50);
+		productionSouhaitee[POUDRE_HQ] = new Indicateur("Production souhaitee de poudre HQ",this,0);
+		productionSouhaitee[FRIANDISES_MQ] = new Indicateur("Production souhaitee de friandises MQ",this,115);
 
-        achatsSouhaites[FEVES_BQ] = new Indicateur("Achats souhaites de feves BQ", this, 360);
-        achatsSouhaites[FEVES_MQ] = new Indicateur("Achats souhaites de feves MQ",this,840);
-        achatsSouhaites[TABLETTES_BQ] = new Indicateur("Achats souhaites de tablettes BQ",this,0);
-        achatsSouhaites[TABLETTES_MQ] = new Indicateur("Achats souhaites de tablettes MQ",this,0);
-        achatsSouhaites[TABLETTES_HQ] = new Indicateur("Achats souhaites de tablettes HQ",this,0);
-        achatsSouhaites[POUDRE_MQ] = new Indicateur("Achats souhaites de poudre MQ",this,0);
-        achatsSouhaites[POUDRE_HQ] = new Indicateur("Achats souhaites de poudre HQ",this,0);
-        achatsSouhaites[FRIANDISES_MQ] = new Indicateur("Achats souhaites de friandises MQ", this, 0);
-        
-        prix[FEVES_BQ] = new Indicateur("Prix de feves BQ", this, 0);
-        prix[FEVES_MQ] = new Indicateur("Prix de feves MQ",this,0);
-        prix[TABLETTES_BQ] = new Indicateur("Prix de tablettes BQ",this,100);
-        prix[TABLETTES_MQ] = new Indicateur("Prix de tablettes MQ",this,100);
-        prix[TABLETTES_HQ] = new Indicateur("Prix de tablettes HQ",this,0);
-        prix[POUDRE_MQ] = new Indicateur("Prix de poudre MQ",this,100);
-        prix[POUDRE_HQ] = new Indicateur("Prix de poudre HQ",this,0);
-        prix[FRIANDISES_MQ] = new Indicateur("Prix de friandises MQ", this, 100);
-        
-        for (int i = 0; i < nbMarchandises; i++) {
-            stocksSouhaites[i] = new Indicateur("Stocks souhaites de " + Marchandises.getMarchandise(i), this, productionSouhaitee[i].getValeur() + achatsSouhaites[i].getValeur());
-            stocks[i] = new Indicateur("Stocks de " + Marchandises.getMarchandise(i), this, stocksSouhaites[i].getValeur()); // on initialise les vrais stocks comme étant ce que l'on souhaite avoir pour la premiere iteration
-        }
+		achatsSouhaites[FEVES_BQ] = new Indicateur("Achats souhaites de feves BQ", this, 360);
+		achatsSouhaites[FEVES_MQ] = new Indicateur("Achats souhaites de feves MQ",this,840);
+		achatsSouhaites[TABLETTES_BQ] = new Indicateur("Achats souhaites de tablettes BQ",this,0);
+		achatsSouhaites[TABLETTES_MQ] = new Indicateur("Achats souhaites de tablettes MQ",this,0);
+		achatsSouhaites[TABLETTES_HQ] = new Indicateur("Achats souhaites de tablettes HQ",this,0);
+		achatsSouhaites[POUDRE_MQ] = new Indicateur("Achats souhaites de poudre MQ",this,0);
+		achatsSouhaites[POUDRE_HQ] = new Indicateur("Achats souhaites de poudre HQ",this,0);
+		achatsSouhaites[FRIANDISES_MQ] = new Indicateur("Achats souhaites de friandises MQ", this, 0);
 
-        banque=new Indicateur("Banque",this,16_000); // environ benefice 2017 sur nombre d'usines
+		prix[FEVES_BQ] = new Indicateur("Prix de feves BQ", this, 0);
+		prix[FEVES_MQ] = new Indicateur("Prix de feves MQ",this,0);
+		prix[TABLETTES_BQ] = new Indicateur("Prix de tablettes BQ",this,100);
+		prix[TABLETTES_MQ] = new Indicateur("Prix de tablettes MQ",this,100);
+		prix[TABLETTES_HQ] = new Indicateur("Prix de tablettes HQ",this,0);
+		prix[POUDRE_MQ] = new Indicateur("Prix de poudre MQ",this,100);
+		prix[POUDRE_HQ] = new Indicateur("Prix de poudre HQ",this,0);
+		prix[FRIANDISES_MQ] = new Indicateur("Prix de friandises MQ", this, 100);
 
-        for (Field field : getClass().getDeclaredFields()) {
-            if(field==null) continue;
-            try {
-                if(field.get(this) instanceof  Indicateur)
-                    Monde.LE_MONDE.ajouterIndicateur((Indicateur) field.get(this));
-                else if(field.get(this) instanceof Indicateur[])
-                    for (Indicateur indicateur : (Indicateur[]) field.get(this))
-                        Monde.LE_MONDE.ajouterIndicateur(indicateur);
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+		for (int i = 0; i < nbMarchandises; i++) {
+			stocksSouhaites[i] = new Indicateur("Stocks souhaites de " + Marchandises.getMarchandise(i), this, productionSouhaitee[i].getValeur() + achatsSouhaites[i].getValeur());
+			stocks[i] = new Indicateur("Stocks de " + Marchandises.getMarchandise(i), this, stocksSouhaites[i].getValeur()); // on initialise les vrais stocks comme étant ce que l'on souhaite avoir pour la premiere iteration
+		}
 
-    @Override
-    public String getNom() {
-        return "Eq5TRAN";
-    }
+		banque=new Indicateur("Banque",this,16_000); // environ benefice 2017 sur nombre d'usines
 
-    @Override
-    public void next() {
+		for (Field field : getClass().getDeclaredFields()) {
+			if(field==null) continue;
+			try {
+				if(field.get(this) instanceof  Indicateur)
+					Monde.LE_MONDE.ajouterIndicateur((Indicateur) field.get(this));
+				else if(field.get(this) instanceof Indicateur[])
+					for (Indicateur indicateur : (Indicateur[]) field.get(this))
+						Monde.LE_MONDE.ajouterIndicateur(indicateur);
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-    }
+	@Override
+	public String getNom() {
+		return "Eq5TRAN";
+	}
+
+	@Override
+	public void next() {
+
+	}
 
 	@Override
 	/**
@@ -112,90 +103,90 @@ public class Eq5TRAN implements Acteur, IAcheteurPoudre, IVendeurPoudre, Ivendeu
 	 */
 	public ContratPoudre[] getCataloguePoudre(IAcheteurPoudre acheteur) {
 		if (stocks[POUDRE_MQ].getValeur()==0) return new ContratPoudre[0];
-		
+
 		ContratPoudre[] catalogue = new ContratPoudre[1];
 		catalogue[0]=new ContratPoudre(1,(int) stocks[POUDRE_MQ].getValeur(),prix[POUDRE_MQ].getValeur(),acheteur,this,false);
 		return catalogue;
-	
+
 	}
 
-    @Override
-    /**
-     * @author Juliette
-     * V1 : on n'envoie un devis que si la qualité demandée est moyenne (la seule que nous vendons) et que nous avons assez de stocks
-     */
-    public ContratPoudre[] getDevisPoudre(ContratPoudre[] demande, IAcheteurPoudre acheteur) {
-        ContratPoudre[] devis = new ContratPoudre[demande.length];
-        for (int i=0; i<demande.length;i++) {
-            if (demande[i].getQualite()!=1 && demande[i].getQuantite()<stocks[POUDRE_MQ].getValeur()) {
-                devis[i]=new ContratPoudre(0,0,0,acheteur,this,false);
-            }
-            else{
-                devis[i]=new ContratPoudre(demande[i].getQualite(), demande[i].getQuantite(), prix[POUDRE_MQ].getValeur(), acheteur, this, false);
-            }
-        }
+	@Override
+	/**
+	 * @author Juliette
+	 * V1 : on n'envoie un devis que si la qualité demandée est moyenne (la seule que nous vendons) et que nous avons assez de stocks
+	 */
+	public ContratPoudre[] getDevisPoudre(ContratPoudre[] demande, IAcheteurPoudre acheteur) {
+		ContratPoudre[] devis = new ContratPoudre[demande.length];
+		for (int i=0; i<demande.length;i++) {
+			if (demande[i].getQualite()!=1 && demande[i].getQuantite()<stocks[POUDRE_MQ].getValeur()) {
+				devis[i]=new ContratPoudre(0,0,0,acheteur,this,false);
+			}
+			else{
+				devis[i]=new ContratPoudre(demande[i].getQualite(), demande[i].getQuantite(), prix[POUDRE_MQ].getValeur(), acheteur, this, false);
+			}
+		}
 
-        return devis;
-    }
+		return devis;
+	}
 
-    @Override
-    /**
-     * @author Juliette
-     * V1 : si la réponse est cohérente avec la demande initiale, nos stocks et nos prix, on répond oui
-     */
-    public void sendReponsePoudre(ContratPoudre[] devis, IAcheteurPoudre acheteur) {
-        ContratPoudre[] reponse = new ContratPoudre[devis.length];
-        for (int i=0; i<devis.length;i++){
-            if (devis[i].getQualite()!=1 && devis[i].getQuantite() < stocks[POUDRE_MQ].getValeur() && devis[i].getPrix() == prix[POUDRE_MQ].getValeur()) {
-                reponse[i] = new ContratPoudre (devis[i].getQualite(), devis[i].getQuantite(), devis[i].getPrix(), devis[i].getAcheteur(), devis[i].getVendeur(), true);
-            }
-            else {
-                reponse[i] = new ContratPoudre (devis[i].getQualite(), devis[i].getQuantite(), devis[i].getPrix(), devis[i].getAcheteur(), devis[i].getVendeur(), false);
-            }
-        }
-    }
+	@Override
+	/**
+	 * @author Juliette
+	 * V1 : si la réponse est cohérente avec la demande initiale, nos stocks et nos prix, on répond oui
+	 */
+	public void sendReponsePoudre(ContratPoudre[] devis, IAcheteurPoudre acheteur) {
+		ContratPoudre[] reponse = new ContratPoudre[devis.length];
+		for (int i=0; i<devis.length;i++){
+			if (devis[i].getQualite()!=1 && devis[i].getQuantite() < stocks[POUDRE_MQ].getValeur() && devis[i].getPrix() == prix[POUDRE_MQ].getValeur()) {
+				reponse[i] = new ContratPoudre (devis[i].getQualite(), devis[i].getQuantite(), devis[i].getPrix(), devis[i].getAcheteur(), devis[i].getVendeur(), true);
+			}
+			else {
+				reponse[i] = new ContratPoudre (devis[i].getQualite(), devis[i].getQuantite(), devis[i].getPrix(), devis[i].getAcheteur(), devis[i].getVendeur(), false);
+			}
+		}
+	}
 
-    @Override
-    /**
-     * @author Juliette
-     * Pour la V1 on suppose que le contrat est entièrement honnoré
-     */
-    public ContratPoudre[] getEchangeFinalPoudre(ContratPoudre[] contrat, IAcheteurPoudre acheteur) {
-    	ContratPoudre[] echangesEffectifs = new ContratPoudre[contrat.length];
-    	for (int i=0;i<contrat.length;i++) {
-    		echangesEffectifs[i]=contrat[i];
-       	}
-        return echangesEffectifs;
-    }
-    
-    /**
-     * @author Juliette
-     * Dans cette méthode, nous sommes ACHETEURS
-     * Methode permettant de récupérer les devis de poudre correspondant à nos demandes et de décider si on les accepte ou non
-     */    
-    private void getTousLesDevisPoudre(ContratPoudre[] demande) {
-    	List<Acteur> listeActeurs = Monde.LE_MONDE.getActeurs();
+	@Override
+	/**
+	 * @author Juliette
+	 * Pour la V1 on suppose que le contrat est entièrement honnoré
+	 */
+	public ContratPoudre[] getEchangeFinalPoudre(ContratPoudre[] contrat, IAcheteurPoudre acheteur) {
+		ContratPoudre[] echangesEffectifs = new ContratPoudre[contrat.length];
+		for (int i=0;i<contrat.length;i++) {
+			echangesEffectifs[i]=contrat[i];
+		}
+		return echangesEffectifs;
+	}
 
-    	List<ContratPoudre[]> devis = new ArrayList<ContratPoudre[]>();
+	/**
+	 * @author Juliette
+	 * Dans cette méthode, nous sommes ACHETEURS
+	 * Methode permettant de récupérer les devis de poudre correspondant à nos demandes et de décider si on les accepte ou non
+	 */    
+	private void getTousLesDevisPoudre(ContratPoudre[] demande) {
+		List<Acteur> listeActeurs = Monde.LE_MONDE.getActeurs();
 
-    	for (Acteur acteur : listeActeurs) {
-    		if(acteur instanceof IVendeurPoudre) {
-    			devis.add(((IVendeurPoudre)acteur).getDevisPoudre(demande, this));
-    		}
-    	}
-    	for(ContratPoudre[] contrat : devis) {
-    		for(int j=0;j<3;j++) {
-    			if(contrat[j].equals(demande[j])) {
-    				contrat[j].setReponse(true);
-    			}
-    		}
-    		contrat[0].getVendeur().sendReponsePoudre(contrat, this);
-    	}
-    						
-    					
-   }
+		List<ContratPoudre[]> devis = new ArrayList<ContratPoudre[]>();
 
-    @Override
+		for (Acteur acteur : listeActeurs) {
+			if(acteur instanceof IVendeurPoudre) {
+				devis.add(((IVendeurPoudre)acteur).getDevisPoudre(demande, this));
+			}
+		}
+		for(ContratPoudre[] contrat : devis) {
+			for(int j=0;j<3;j++) {
+				if(contrat[j].equals(demande[j])) {
+					contrat[j].setReponse(true);
+				}
+			}
+			contrat[0].getVendeur().sendReponsePoudre(contrat, this);
+		}
+
+
+	}
+
+	@Override
 	public double getReponse(DemandeAO d) {
 		switch(d.getQualite()) {
 		case 1: return Double.MAX_VALUE;
@@ -208,5 +199,5 @@ public class Eq5TRAN implements Acteur, IAcheteurPoudre, IVendeurPoudre, Ivendeu
 		return Double.MAX_VALUE;
 	} /** @Author: Maxim **/
 
-	
+
 }
