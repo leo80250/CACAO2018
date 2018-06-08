592eeb8e90584cba91db549bb74176b8563c43bd
Agathe Chevalier <Agathe Chevalier@DESKTOP-TF7M9AT>
2018-06-08 14:49:26 +0200

    changer signe inégalité

--- CACAO2018/src/abstraction/eq2PROD/Eq2PROD.java
+++ CACAO2018/src/abstraction/eq2PROD/Eq2PROD.java
@@ -172,7 +172,7 @@ public class Eq2PROD implements Acteur, IVendeurFeve, IVendeurFevesProd {
 	 * car il est impossible que cela arrive
 	 */
 	public int acheter(int quantite) {
-		if (quantite >= this.stockQB) {
+		if (quantite <= this.stockQB) {
 			this.stockQB=this.stockQB - quantite ;
 			this.solde = this.solde /*+ quantite*MarcheFeve.getPrixMarche()*this.coeffStock */ ;
 			return quantite ;
