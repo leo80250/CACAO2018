6814c3b5d29a3be1da9583b9061069d320624994
Agathe Chevalier <Agathe Chevalier@DESKTOP-TF7M9AT>
2018-06-08 15:35:07 +0200

    erreur getJournal()

--- CACAO2018/src/abstraction/eq2PROD/Eq2PROD.java
+++ CACAO2018/src/abstraction/eq2PROD/Eq2PROD.java
@@ -111,10 +111,10 @@ public class Eq2PROD implements Acteur, IVendeurFeve, IVendeurFevesProd {
 		this.stockQM=this.stockQM+ (int) (this.coeffStock*MOY_QM);
 		this.stockQB=this.stockQB+ (int) (this.coeffStock*MOY_QB);
 		this.solde=this.solde-coutFixe;
-		this.getJournal().ajouter("Quantité basse qualité = "+ this.getStockQB());
+		/*this.getJournal().ajouter("Quantité basse qualité = "+ this.getStockQB());
 		this.getJournal().ajouter("Quantité moyenne qualité ="+ this.getStockQM());
 		this.getJournal().ajouter("Coefficient de la météo ="+ this.getCoeffMeteo());
-		this.getJournal().ajouter("Coefficient des maladies ="+ this.getCoeffMaladie());
+		this.getJournal().ajouter("Coefficient des maladies ="+ this.getCoeffMaladie());*/
 	}
 
 	/* Code par Guillaume SALLE+Romain BERNARD+Agathe CHEVALIER */
