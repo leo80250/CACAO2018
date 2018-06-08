2bfa02eedd402e08c66b196ab0f61a97b462df57
clair <clair@172.18.103.49>
2018-06-08 16:17:26 +0200

    a

--- CACAO2018/src/abstraction/eq3PROD/Eq3PROD.java
+++ CACAO2018/src/abstraction/eq3PROD/Eq3PROD.java
@@ -2,44 +2,74 @@ package abstraction.eq3PROD;
 
 import abstraction.eq3PROD.echangesProdTransfo.ContratFeve;
 import abstraction.eq3PROD.echangesProdTransfo.IAcheteurFeve;
-import abstraction.eq3PROD.echangesProdTransfo.IMarcheFeve;
-import abstraction.eq4TRAN.IVendeurChoco;
-import abstraction.eq4TRAN.VendeurChoco.GPrix;
-import abstraction.eq4TRAN.VendeurChoco.GQte;
-import abstraction.eq7TRAN.echangeTRANTRAN.ContratPoudre;
-import abstraction.eq7TRAN.echangeTRANTRAN.IAcheteurPoudre;
-import abstraction.eq7TRAN.echangeTRANTRAN.IVendeurPoudre; 
 import abstraction.fourni.Acteur;
 import abstraction.fourni.Indicateur;  
 import abstraction.fourni.Journal;
 import abstraction.fourni.Monde;
+import abstraction.eq3PROD.echangesProdTransfo.IMarcheFeve;
+import abstraction.eq3PROD.echangesProdTransfo.MarcheFeve;
 
 //import abstraction.eq1DIST.IVenteConso;
 
 public class Eq3PROD implements Acteur, abstraction.eq3PROD.echangesProdTransfo.IVendeurFeve {
-	private Indicateur achats;
-	private Indicateur ventes;
 	// 0 = BQ, 1 = MQ, 2 = HQ
+	private String nom;
 	private Indicateur[] stockFeves;
-	private Indicateur[] stockPoudre;
-	private Indicateur[] stockTablettes;
+	private Indicateur[] prixVentesFeves;
+	private Indicateur ventesfines;
+	private Indicateur ventesmoyennes;
 	private Indicateur solde;
+	private Indicateur[] productionFeves;
 	private Journal journal;
 	
 	private int stockmoyen;
 	private int stockfin;
+	
 	private ContratFeve[] eq4;
 	private ContratFeve[] eq5;
 	private ContratFeve[] eq7;
 	private ContratFeve[] virtuel;
-	/*private int tpsnonmaladieIndo;
-	private int tpsnonmaladieAmerique;*/
+	private final double[] prix_Ventes_Feves = {1800, 2100, 2500};
+	private final double[] prodFeves = {0,0,0};
 	
-	
-	
+	/**
+	 * @author Claire
+	 */
 	public Eq3PROD() {
 		this.stockmoyen= 75000;
 		this.stockfin= 24000;
+		this.nom = nom;
+		this.ventesfines = new Indicateur("Vente fines de "+this.getNom(), this, 0.0);
+		this.ventesmoyennes = new Indicateur("Ventes moyennes de"+this.getNom(),this, 0.0);
+		this.stockFeves = new Indicateur[3];
+		this.prixVentesFeves = new Indicateur[3];
+		this.productionFeves = new Indicateur[3];
+		
+		this.solde = new Indicateur(this.getNom()+" a un solde de ", this, 0.0);
+		
+		for(int i = 0; i < 3; i++) {
+			String s="";
+			if (i==0) {
+				s="basses";
+			}
+			else if(i==1) {
+				s="moyennes";
+			}
+			else {
+				s="fines";
+			}
+			this.stockFeves[i] = new Indicateur(this.getNom()+" a un stock de fèves "+s+" de ", this, 0.0);
+			this.prixVentesFeves[i] = new Indicateur(this.getNom()+" a dernièrement acheté des fèves "+s+" au prix de ", this, this.prix_Ventes_Feves[i]);
+			this.productionFeves[i] = new Indicateur(this.getNom()+" a dernièrement produit une quantité de feves "+s+" de", this, prodFeves[i] );
+			}
+		
+		this.journal = new Journal("Journal de "+this.getNom());
+		Monde.LE_MONDE.ajouterJournal(this.journal);
+		Monde.LE_MONDE.ajouterIndicateur(this.ventesmoyennes);
+		Monde.LE_MONDE.ajouterIndicateur(this.ventesfines);
+		Monde.LE_MONDE.ajouterIndicateur(this.stockFeves[1]);
+		Monde.LE_MONDE.ajouterIndicateur(this.stockFeves[2]);
+		Monde.LE_MONDE.ajouterIndicateur(this.solde);
 
 	}
 	
@@ -102,6 +132,7 @@ public class Eq3PROD implements Acteur, abstraction.eq3PROD.echangesProdTransfo.
 		**/
 		public void next() {
 			int x=Monde.LE_MONDE.getStep();
+			//double k=MarcheFeve.getPrixMarche();
 			int prodBresil=0;
 			int prodIndo=0;
 			int prodfin=0;
@@ -144,10 +175,11 @@ public class Eq3PROD implements Acteur, abstraction.eq3PROD.echangesProdTransfo.
 			}
 			this.stockmoyen = this.stockmoyen+prodBresil+prodIndo;
 			this.stockfin = this.stockfin+prodfin;
+			this.prodFeves[1]=prodBresil+prodIndo;
+			this.prodFeves[2]=prodfin;
 			System.out.println(" eq 3 production feve moyennes de "+(prodBresil+prodIndo)+" --> stockMoyen="+this.stockmoyen);
 			System.out.println("eq 3 production feve fines de "+prodfin+" --> stockFin="+this.stockfin);
-			/*vendeur = (Monde.LE_MONDE.getActeur("Eq6DIST"));
-			vendeur.sell(100);*/
+			
 		}
 
 		@Override
