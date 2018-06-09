02804113bfe7cab579f34ba1826f74654e1ead20
ThomasSchillaci <thomas.schillaci@gmail.com>
2018-06-09 19:35:36 +0200

    Debug

---
+++ CACAO2018/src/abstraction/eq6DIST/Eq6DIST.java
@@ -0,0 +1,92 @@
+package abstraction.eq6DIST;
+
+import java.util.ArrayList;
+
+import abstraction.eq1DIST.IVenteConso;
+import abstraction.eq4TRAN.ITransformateur;
+import abstraction.eq4TRAN.VendeurChoco.GPrix;
+import abstraction.eq4TRAN.VendeurChoco.GQte;
+import abstraction.fourni.Acteur;
+import abstraction.fourni.Indicateur;
+import abstraction.fourni.Monde;
+
+public class Eq6DIST implements Acteur{ //IAcheteurChoco {
+	
+	private Indicateur stock_BBQ;
+	private Indicateur stock_BMQ;
+	private Indicateur stock_BHQ;
+	private Indicateur stock_TBQ;
+	private Indicateur stock_TMQ;
+	private Indicateur stock_THQ;
+	private Indicateur banque;
+	private GQte stock;
+	public Eq6DIST() {
+		//MarcheChoco MC = new MarcheChoco();
+		//Monde.LE_MONDE.ajouterActeur(MC);
+		this.banque= new Indicateur("Solde bancaire Eq6 : ",this, 120000);
+		this.stock= new GQte(0,0,0,0,0,0);	
+		this.stock_BBQ= new Indicateur("Stock de bonbons BQ Eq6 :",this);
+		this.stock_BMQ=new Indicateur("Stock de bonbons MQ Eq6 :",this);
+		this.stock_BHQ=new Indicateur("Stock de bonbons HQ Eq6 :",this);
+		this.stock_TBQ=new Indicateur("Stock de tablettes BQ Eq6 :",this);
+		this.stock_TMQ=new Indicateur("Stock de tablettes MQ Eq6 :",this);
+		this.stock_THQ=new Indicateur("Stock de tablettes HQ Eq6 :",this);
+		Monde.LE_MONDE.ajouterIndicateur(this.banque);
+		Monde.LE_MONDE.ajouterIndicateur(this.stock_BBQ);
+		Monde.LE_MONDE.ajouterIndicateur(this.stock_BMQ);
+		Monde.LE_MONDE.ajouterIndicateur(this.stock_BHQ);
+		Monde.LE_MONDE.ajouterIndicateur(this.stock_TBQ);
+		Monde.LE_MONDE.ajouterIndicateur(this.stock_TMQ);
+		Monde.LE_MONDE.ajouterIndicateur(this.stock_THQ);
+	}
+	@Override
+	public String getNom() {
+		// TODO Auto-generated method stub;
+		return "Eq6DIST";
+	}
+
+	@Override
+	public void next() {
+		// TODO Auto-generated method stub;
+		this.stock_BBQ.setValeur(this, this.stock.getqBonbonBQ());
+		this.stock_BMQ.setValeur(this, this.stock.getqBonbonMQ());
+		this.stock_BHQ.setValeur(this, this.stock.getqBonbonHQ());
+		this.stock_TBQ.setValeur(this, this.stock.getqTabletteBQ());
+		this.stock_TMQ.setValeur(this, this.stock.getqTabletteMQ());
+		this.stock_THQ.setValeur(this, this.stock.getqTabletteHQ());
+		
+		
+	}
+	
+	
+	public ArrayList<GQte> getCommande(ArrayList<GPrix> gPrix, ArrayList<GQte> stock) {
+		/**
+		 * Karel Kédémos
+		 */
+		ArrayList<GQte> commande = new ArrayList<GQte>();
+		
+		final int quantité_demandée_transfo1_CMG=0;
+		final int quantité_demandée_transfo1_TBG=0;
+		final int quantité_demandée_transfo1_TMG=3000;
+		final int quantité_demandée_transfo2_CMG=24650;
+	 	final int quantité_demandée_transfo2_TBG=4000;
+		final int quantité_demandée_transfo2_TMG=48000;
+		final int quantité_demandée_transfo3_CMG=5800;
+		final int quantité_demandée_transfo3_TBG=200;
+		final int quantité_demandée_transfo3_TMG=6750;
+		
+		commande.add(new GQte(0, quantité_demandée_transfo1_CMG,0,quantité_demandée_transfo1_TBG,quantité_demandée_transfo1_TMG,0));
+		commande.add(new GQte(0, quantité_demandée_transfo2_CMG,0,quantité_demandée_transfo2_TBG,quantité_demandée_transfo2_TMG,0));
+		commande.add(new GQte(0, quantité_demandée_transfo3_CMG,0,quantité_demandée_transfo3_TBG,quantité_demandée_transfo3_TMG,0));
+		return(commande);
+	}
+	/*public void livraison(GQte d) {
+		this.stock.setqBonbonBQ(this.stock.getqBonbonBQ()+d.getqBonbonBQ());
+		this.stock.setqBonbonMQ(this.stock.getqBonbonMQ()+d.getqBonbonMQ());
+		this.stock.setqBonbonHQ(this.stock.getqBonbonHQ()+d.getqBonbonHQ());
+		this.stock.setqTabletteBQ(this.stock.getqTabletteBQ()+d.getqTabletteBQ());
+		this.stock.setqTabletteMQ(this.stock.getqTabletteMQ()+d.getqTabletteMQ());
+		this.stock.setqTabletteHQ(this.stock.getqTabletteHQ()+d.getqTabletteHQ());
+	}*/
+	
+}
---
+++ CACAO2018/src/abstraction/eq6DIST/MarcheChoco.java
@@ -0,0 +1,103 @@
+package abstraction.eq6DIST; // Karel Kédémos , Victor Signes, Léopold Petitjean
+import java.util.ArrayList;
+
+import abstraction.eq4TRAN.IVendeurChoco;
+import abstraction.eq4TRAN.VendeurChoco.GPrix;
+import abstraction.eq4TRAN.VendeurChoco.GQte;
+import abstraction.fourni.Acteur;
+import abstraction.fourni.Monde;
+
+public class MarcheChoco  implements Acteur{
+	private ArrayList<GQte> stock;
+	private ArrayList<GPrix> prix;
+	private ArrayList <Acteur> distributeurs;
+	private ArrayList <Acteur> transformateurs;
+	
+	public MarcheChoco() {
+		
+	this.distributeurs= new ArrayList<Acteur>();
+	this.distributeurs.add((Monde.LE_MONDE.getActeur("Eq6DIST")));
+	this.distributeurs.add((Monde.LE_MONDE.getActeur("Eq1DIST")));
+	this.transformateurs= new ArrayList<Acteur>();
+	this.transformateurs.add( Monde.LE_MONDE.getActeur("Eq4TRAN"));
+	this.transformateurs.add( Monde.LE_MONDE.getActeur("Eq5TRAN"));
+	this.transformateurs.add( Monde.LE_MONDE.getActeur("Eq7TRAN"));
+	this.stock= new ArrayList <GQte>();
+	this.prix= new ArrayList <GPrix>();
+	
+	
+	for (Acteur i : this.transformateurs) {
+		IVendeurChoco ibis= (IVendeurChoco) i;
+		this.prix.add(ibis.getPrix());
+		this.stock.add(ibis.getStock());
+			
+	}	
+	}
+	public void next() {
+		ArrayList<ArrayList<GQte>> commande=  new ArrayList<ArrayList<GQte>>();
+		for (Acteur i : this.distributeurs) {
+			IAcheteurChoco ibis = (IAcheteurChoco) i;
+			commande.add(ibis.getCommande(this.prix, this.stock));
+		}
+		ArrayList<ArrayList<GQte>> livraison = new ArrayList<ArrayList<GQte>>();
+		for(int j =0; j<3;j++) {
+			ArrayList<GQte> Livraisoni =new ArrayList<GQte>(); 
+			int qBonbonBQj=0;
+			int qBonbonMQj=0;
+			int qBonbonHQj=0;
+			int qTabletteBQj=0;
+			int qTabletteMQj=0;
+			int qTabletteHQj=0;
+			for (int i=0 ; i<commande.size() ; i++) {
+				qBonbonBQj=qBonbonBQj+commande.get(i).get(j).getqBonbonBQ();
+				qBonbonMQj=qBonbonMQj+commande.get(i).get(j).getqBonbonMQ();
+				qBonbonHQj=qBonbonHQj+commande.get(i).get(j).getqBonbonHQ();
+				qTabletteBQj=qTabletteBQj+commande.get(i).get(j).getqTabletteBQ();
+				qTabletteMQj=qTabletteMQj+commande.get(i).get(j).getqTabletteMQ();
+				qTabletteHQj=qTabletteHQj+commande.get(i).get(j).getqTabletteHQ();
+				
+				Livraisoni.add(new GQte(qBonbonBQj,qBonbonMQj,qBonbonHQj,qTabletteBQj,qTabletteMQj,qTabletteHQj) );
+			}
+			livraison.add(Livraisoni);		
+		}
+		int l=0;
+		ArrayList<ArrayList<GQte>> Delivery = new ArrayList<ArrayList<GQte>>();
+		for (Acteur i : this.transformateurs)	{
+			IVendeurChoco ibis = (IVendeurChoco) i;
+			//Delivery.add(ibis.getLivraison(livraison.get(l)));
+			l++;
+		}
+		l=0;
+		ArrayList<GQte> PourDIST=new ArrayList<GQte>();
+		for (int j=0;j<2;j++) {
+			int qBonbonBQj=0;
+			int qBonbonMQj=0;
+			int qBonbonHQj=0;
+			int qTabletteBQj=0;
+			int qTabletteMQj=0;
+			int qTabletteHQj=0;
+			for (int i=0; i<Delivery.size(); i++) {
+				qBonbonBQj=qBonbonBQj+Delivery.get(i).get(j).getqBonbonBQ();
+				qBonbonMQj=qBonbonMQj+Delivery.get(i).get(j).getqBonbonMQ();
+				qBonbonHQj=qBonbonHQj+Delivery.get(i).get(j).getqBonbonHQ();
+				qTabletteBQj=qTabletteBQj+Delivery.get(i).get(j).getqTabletteBQ();
+				qTabletteMQj=qTabletteMQj+Delivery.get(i).get(j).getqTabletteMQ();
+				qTabletteHQj=qTabletteHQj+Delivery.get(i).get(j).getqTabletteHQ();
+				GQte Deliveryj= new GQte(qBonbonBQj,qBonbonMQj,qBonbonHQj,qTabletteBQj,qTabletteMQj,qTabletteHQj);
+				PourDIST.add(Deliveryj);
+			}
+			
+			}
+		for (Acteur i : this.distributeurs) {
+			IAcheteurChoco ibis = (IAcheteurChoco) i;
+			//ibis.livraison(PourDIST.get(l));
+			l++;
+		}
+	}
+	@Override
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return "MarcheChoco";
+	}
+	
+}
