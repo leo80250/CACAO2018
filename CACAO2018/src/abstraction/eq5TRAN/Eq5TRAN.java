c09960031548612787980e58b6ff9d728ecefef6
Juliette <Juliette@172.17.3.241>
2018-06-22 16:51:09 +0200

    prix de vente

--- CACAO2018/src/abstraction/eq5TRAN/Eq5TRAN.java
+++ CACAO2018/src/abstraction/eq5TRAN/Eq5TRAN.java
@@ -82,13 +82,13 @@ public class Eq5TRAN implements Acteur, IAcheteurPoudre, IVendeurPoudre, Ivendeu
 
 		prix[FEVES_BQ] = new Indicateur("Eq5 - Prix de feves BQ", this, 0);
 		prix[FEVES_MQ] = new Indicateur("Eq5 - Prix de feves MQ", this, 0);
-		prix[TABLETTES_BQ] = new Indicateur("Eq5 - Prix de tablettes BQ", this, 4800);
-		prix[TABLETTES_MQ] = new Indicateur("Eq5 - Prix de tablettes MQ", this, 10000);
-		prix[TABLETTES_HQ] = new Indicateur("Eq5 - Prix de tablettes HQ", this, 0);
+		prix[TABLETTES_BQ] = new Indicateur("Eq5 - Prix de tablettes BQ", this, 8000000);
+		prix[TABLETTES_MQ] = new Indicateur("Eq5 - Prix de tablettes MQ", this, 10000000);
+		prix[TABLETTES_HQ] = new Indicateur("Eq5 - Prix de tablettes HQ", this, 18000000);
 		prix[POUDRE_BQ] = new Indicateur("Eq5 - Prix de poudre BQ", this, 0);
-		prix[POUDRE_MQ] = new Indicateur("Eq5 - Prix de poudre MQ", this, 100);
+		prix[POUDRE_MQ] = new Indicateur("Eq5 - Prix de poudre MQ", this, 8500000);
 		prix[POUDRE_HQ] = new Indicateur("Eq5 - Prix de poudre HQ", this, 0);
-		prix[FRIANDISES_MQ] = new Indicateur("Eq5 - Prix de friandises MQ", this, 8000);
+		prix[FRIANDISES_MQ] = new Indicateur("Eq5 - Prix de friandises MQ", this, 8000000);
 
 		for (int i = 0; i < nbMarchandises; i++) {
 			stocksSouhaites[i] = new Indicateur("Eq5 - Stocks souhaites de " + Marchandises.getMarchandise(i), this, productionSouhaitee[i].getValeur() + achatsSouhaites[i].getValeur());
