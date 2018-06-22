b61dc536be313a00f6d39522f2ce063ec6b27199
Maxim <Maxim@172.18.122.26>
2018-06-22 16:27:13 +0200

    Finalisation de IvendeurOccasionnelChoco + implémentation

--- CACAO2018/src/abstraction/eq5TRAN/Eq5TRAN.java
+++ CACAO2018/src/abstraction/eq5TRAN/Eq5TRAN.java
@@ -441,6 +441,14 @@ public class Eq5TRAN implements Acteur, IAcheteurPoudre, IVendeurPoudre, Ivendeu
         }
         return Double.MAX_VALUE;
     }
+    
+    /** @author Maxim */
+
+	@Override
+	public void envoyerReponse(double quantite, int qualite, int prix) {
+		this.depenser(-prix);
+		this.stocks[qualite].setValeur(this, this.stocks[qualite].getValeur()-quantite);
+	}
 
 
 }
