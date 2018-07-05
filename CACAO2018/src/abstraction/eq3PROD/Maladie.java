560abbfe641c6315320c4c6b99f44012e442f40c
Grégoire <Grgoire@172.17.3.231>
2018-07-05 10:11:57 +0200

    Faîtes joujou avec les maladies

--- CACAO2018/src/abstraction/eq3PROD/Maladie.java
+++ CACAO2018/src/abstraction/eq3PROD/Maladie.java
@@ -8,22 +8,27 @@ public class Maladie {
 	public double pertesProd;
 	public int maladieActive;
 	public int dureeMax;
+	public String nom;
 	
-	public Maladie(double probaD, double pertesProd, int dureeMax) {
+	public Maladie(double probaD, double pertesProd, int dureeMax, String nom) {
 		this.probaD = probaD;
 		this.pertesProd = pertesProd;
 		this.dureeMax = dureeMax;
 		this.maladieActive = 0;
+		this.nom = nom;
 	}
-	
-	public void setMaladieActive() {
-		Random r = new Random();
-		this.maladieActive = r.nextInt(dureeMax);
-	}
-	
+
 	public boolean declencherMaladie() {
 		return (this.maladieActive == 0 && Math.random() < this.probaD);
 	}
+	
+	public void setMaladieActive() {
+		if (this.declencherMaladie()) {
+			Random r = new Random();
+			this.maladieActive = r.nextInt(dureeMax);
+		}
+	}
+	
 	
 	public double pertesMaladie() {
 		if (this.maladieActive > 0) {
@@ -31,6 +36,11 @@ public class Maladie {
 			return (1.0-this.pertesProd);
 		}
 		return 1.0;
+	}
+	
+	public String toString() {
+		return "Nom : "+this.nom+"\nMaladie active pour "+this.maladieActive+" steps\n"
+				+"Pertes : "+this.pertesMaladie()+"\n--------------";
 	}
 	
 }
--- CACAO2018/src/abstraction/eq3PROD/TestMaladies.java
+++ CACAO2018/src/abstraction/eq3PROD/TestMaladies.java
@@ -3,7 +3,12 @@ package abstraction.eq3PROD;
 public class TestMaladies {
 	
 	public static void main(String[] args) {
+		Maladie maladie = new Maladie(0.042, 0.10, 4, "Flemmingite aigüe");
 		
+		for (int i = 0 ; i < 500 ; i++) {
+			maladie.setMaladieActive();
+			if (maladie.maladieActive > 0) System.out.println("Step "+i+"\n"+maladie.toString());
+		}
 	}
 
 }
