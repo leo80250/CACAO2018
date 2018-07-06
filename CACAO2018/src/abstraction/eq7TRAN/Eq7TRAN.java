1e4a2bace47cd7e2ad073234965e3769aaf142a5
MaelleBoulard <maelle.boulard@orange.fr>
2018-07-06 15:20:04 +0200

    10

--- CACAO2018/src/abstraction/eq7TRAN/Eq7TRAN.java
+++ CACAO2018/src/abstraction/eq7TRAN/Eq7TRAN.java
@@ -1159,6 +1159,8 @@ public class Eq7TRAN implements Acteur, IAcheteurPoudre, IVendeurPoudre, IAchete
 	/////////////////////////////////////
 	
 	/**
+	 * 
+	 * 
 	 * @author bernardjoseph  
 	 * @param qualite
 	 */
