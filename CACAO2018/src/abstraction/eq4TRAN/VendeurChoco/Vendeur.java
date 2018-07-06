796bee4de332a3000c9bbb95c36b50b6dd860390
Charles <Charles@172.18.135.178>
2018-07-06 14:57:48 +0200

    vbejs

--- CACAO2018/src/abstraction/eq4TRAN/VendeurChoco/Vendeur.java
+++ CACAO2018/src/abstraction/eq4TRAN/VendeurChoco/Vendeur.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-import abstraction.eq4TRAN.IVendeurChoco;
 import abstraction.eq4TRAN.IVendeurChocoBis;
 import abstraction.fourni.Journal;
 
